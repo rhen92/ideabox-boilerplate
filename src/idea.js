3b609d28113d6613e0ae9a5cecef589d144e3195
Jim Charnesky <mijnesky@hotmail.com>
2021-02-24 16:40:49 -0700

    Add methods on class

--- src/idea.js
+++ src/idea.js
@@ -2,7 +2,19 @@ class Idea {
   constructor(title, body) {
     this.title = title;
     this.body = body;
+    this.id = Date.now();
+    this.star = true;
   }
 
+  saveToStorage() {
 
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateIdea() {
+
+  }
 }

77d23bd8adf2d31e0be2fcdff058b0a5235bd4f8
Jim Charnesky <mijnesky@hotmail.com>
2021-02-24 17:20:33 -0700

    Add event listeners for inputs and buttons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 // Query Selectors
+var titleInput = document.querySelector('#titleInput');
+var bodyInput = document.querySelector('#bodyInput');
+var saveButton = document.querySelector('#saveButton');
+var searchInput = document.querySelector('#searchInput');
+var searchButton = document.querySelector('#searchButton');
 
 
 
